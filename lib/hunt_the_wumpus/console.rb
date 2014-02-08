228da9d8dc1808e75fa3ff883716d38fb1bea049
Olga Grabek <ola@codequest.eu>
2014-02-08 16:48:03 +0100

    implement narrator and console class

--- lib/hunt_the_wumpus/console.rb
+++ lib/hunt_the_wumpus/console.rb
@@ -18,6 +18,35 @@ module HuntTheWumpus
 			narrator.say "Exits go to: #{player.room.exits.map(&:number)}"
 		end
 
+		def ask_player_to_act
+			actions = {'m' => :move, 's' => :shoot, 'i' => :inspect}	
+			
+			accepting_player_input do |command, room_number|
+				player.act actions[command], player.room.neighbor(room_number)
+			end
+		end
+
+		private
+
+		def accepting_player_input
+			narrator.say '-----------------------------------------'
+			command = narrator.ask 'What do you want to do ? (m)ove or (shoot) ?'
+
+			unless ['m', 's'].include?(command)
+				narrator.say 'Invalid action! Try again.'
+				return
+			end
+
+			dest = narrator.ask('Where?').to_i
+
+			unless player.room.exits.include?(dest)
+				narrator.say 'You do not see such room.'
+				return
+			end
+
+			yield(command, dest)
+		end
+
 	end
 
 end
--- lib/hunt_the_wumpus/narrator.rb
+++ lib/hunt_the_wumpus/narrator.rb
@@ -2,8 +2,34 @@ module HuntTheWumpus
 
 	class Narrator
 
+		attr_accessor :ending_message
+
 		def say(text)
 			$stdout.puts text
+		end
+
+		def ask(question)
+			print "#{question}"
+			$stdin.gets.chomp 	
+		end
+
+		def tell_story
+			yield until finished?
+				
+			say '-----------------------------------------'
+			describe_ending
+		end
+
+		def finish_story(message)
+			ending_message = message	
+		end
+
+		def finished?
+			!!ending_message
+		end
+
+		def describe_ending
+			say ending_message
 		end
 		
 	end
